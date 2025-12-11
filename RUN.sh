c70de3202149b4739413c59121e9962258b2cef2
Lucas von Corvin <12435611@student.tuwien.ac.at>
2025-12-11 15:07:47 +0100

    add shell script

---
+++ RUN.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[ -d results ] || mkdir results
+
+tol=1e-6
+max_iters=1000000
+
+for ((i=3;i<5;i++));do
+   nx=$((2**i+1))
+   odir=`printf "results/NX%04d" ${nx}`
+   [ -d ${odir} ] || mkdir ${odir}
+   ./JACOBI2D ${nx} ${tol} ${max_iters} ${odir}
+done
